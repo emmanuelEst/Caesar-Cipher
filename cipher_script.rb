37cca5e6604f403267e548699c97d151b1f5b4c2
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-10-14 09:39:24 -0500

    Add mixed case handling when shifting high numbers
    Several changes to methods were made so the initial split string is
    passed as an argument
    Allows the initial split string to be compared with the shifted ordinal
    integer
    
    The each with index method is called so the map method can use the index
    of the shifted ordinal array
    
    Two new methods were created to check whether the original character was
    lower or upper case
    This returns a boolean which is used in the overflow handling so the
    cases are not mixed

--- cipher_script.rb
+++ cipher_script.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 def caesar_cipher(string, shift_int)
-  shift_character_ord = shift_character_ord(string, shift_int)
-  valid_alpha_int = valid_alpha_int(shift_character_ord)
+  split_string = string.split('')
+  shift_character_ord = shift_character_ord(split_string, shift_int)
+  valid_alpha_int = valid_alpha_int(shift_character_ord, split_string)
   valid_alpha_int.map(&:chr).join('')
 end
 
 # returns an array containing shifted ordinal integer
-def shift_character_ord(string, shift_int)
-  split_string = string.split('')
+def shift_character_ord(split_string, shift_int)
   split_string.map(&:ord).map do |char|
     if char == 32
       char
@@ -18,14 +18,27 @@ def shift_character_ord(string, shift_int)
   end
 end
 
-def valid_alpha_int(shifted_array)
-  shifted_array.map do |int|
-    if int > 90 && int < 97
+# checks if an ordinal integer overflows from each lower and upper case range
+def valid_alpha_int(shifted_array, split_string)
+  shifted_array.each_with_index.map do |int, index|
+    if upper?(split_string, index) && int > 90
       64 + (int - 90)
-    elsif int > 122
+    elsif lower?(split_string, index) && int > 122
       96 + (int - 122)
     else
       int
     end
   end
+end
+
+# method checks if the character was uppercase before changing to an ordinal integer
+def upper?(split_string, index)
+  regex = /[[:upper:]]/
+  regex.match?(split_string[index])
+end
+
+# method checks if the character was lowercase before changing to an ordinal integer
+def lower?(split_string, index)
+  regex = /[[:lower:]]/
+  regex.match?(split_string[index])
 end
